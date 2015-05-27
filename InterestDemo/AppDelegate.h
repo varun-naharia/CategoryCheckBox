7f5722789c659284b36ab9bd66043748ba2df67f
Pranay Mathur <gaurav.vachhani@wedigtech.com>
2015-05-27 12:46:11 +0530

    Initial Commit

---
+++ InterestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InterestDemo
+//
+//  Created by Pranay Mathur on 5/27/15.
+//  Copyright (c) 2015 Wedig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InterestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InterestDemo
+//
+//  Created by Pranay Mathur on 5/27/15.
+//  Copyright (c) 2015 Wedig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
