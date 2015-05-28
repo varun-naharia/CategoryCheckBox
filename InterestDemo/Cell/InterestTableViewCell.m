edd9be2afc6075ae94a4d628d194d32336cf644d
varun <varun.naharia@gmail.com>
2015-05-28 18:41:49 +0530

    changes

---
+++ InterestDemo/Cell/InterestTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  InterestTableViewCell.m
+//  InterestDemo
+//
+//  Created by Pranay Mathur on 5/27/15.
+//  Copyright (c) 2015 Wedig. All rights reserved.
+//
+
+#import "InterestTableViewCell.h"
+
+@implementation InterestTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
